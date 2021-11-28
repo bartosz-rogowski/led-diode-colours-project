4f120c50634d9ee45c9050c199d1d30a076d27aa
bartosz-rogowski <bartoz3rogowski@gmail.com>
2021-11-28 20:52:10 +0100

    added some functions and main structure

--- raspberry.py
+++ raspberry.py
@@ -1,28 +1,67 @@
-#!/usr/bin/env/python3
+# !/usr/bin/env/python3
 
 import serial
-import sys
+from sys import exit
 import time
 import colorsys
+from the_vault \
+	import get_average_colour_from_screen, print_menu, clear_console
 
 if __name__ == '__main__':
-    
-    (r, g, b) = colorsys.hsv_to_rgb(1.0, 1.0, 1.0)
-    
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser.reset_input_buffer()
-    switch = ['1', '2']
-    while True:
-        try:
-            if ser.in_waiting > 0:
-                line = ser.readline().decode('utf-8').rstrip()
-                print(line)
-            #for char in switch:
-                #ser.write(char.encode())
-                #line = ser.readline().decode('utf-8').rstrip()
-                #print(line)
-                #time.sleep(1)
-        
-        except KeyboardInterrupt:
-            print("Zamykamy")
-            sys.exit(0)
+	
+	# ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+	# ser.reset_input_buffer()
+	# switch = ['1', '2']
+	while True:
+		try:
+			clear_console()
+			print_menu()
+			choice = int(input('Enter your choice: '))
+			# if ser.in_waiting > 0:
+				# line = ser.readline().decode('utf-8').rstrip()
+				# print(line)
+			#for char in switch:
+				#ser.write(char.encode())
+				#line = ser.readline().decode('utf-8').rstrip()
+				#print(line)
+				# time.sleep(1)
+			if choice == 0:
+				exit(0)
+			if choice == 1:
+				clear_console()
+				print("AVERAGE SCREEN COLOUR MODE")
+				while True:
+					try:
+						colours = get_average_colour_from_screen()
+						print(colours)
+					except KeyboardInterrupt:
+						break
+
+
+			if choice == 2:
+				clear_console()
+				print("LIGHT SENSOR MODE")
+				while True:
+					try:
+						print("Blank Space (for now)")
+						time.sleep(10)
+					except KeyboardInterrupt:
+						break
+
+
+			if choice == 3:
+				clear_console()
+				print("RAINBOW MODE")
+				while True:
+					try:
+						print("Blank Space (for now)")
+						time.sleep(10)
+					except KeyboardInterrupt:
+						break
+	
+	
+		except KeyboardInterrupt:
+			print("\nStopped by user")
+			exit(0)
+		except Exception:
+			pass
---
+++ the_vault.py
@@ -0,0 +1,70 @@
+'''
+This module contains tools and algorithms helpful in project.
+'''
+import numpy as np
+from PIL import ImageGrab
+import time
+import os
+
+def print_menu():
+    '''Function printing menu on terminal.
+    '''
+    print("\t\tMain Menu:\n")
+    print("0 - Exit")
+    print("1 - Set diode colour to average one from the screen")
+    print("2 - Set diode colour to one detected with light sensor")
+    print("3 - Set diode colour to RAINBOW MODE")
+    print()
+    print("-"*44)
+    print("In order to exit from any mode, press Ctrl+C")
+    print("-"*44)
+    print()
+
+def clear_console():
+    '''Simple function clearing console (multiplatform version)
+    '''
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def most_frequent(List):
+	counter = 0
+	num = List[0] 
+	  
+	for i in List: 
+		curr_frequency = List.count(i) 
+		if(curr_frequency > counter): 
+			counter = curr_frequency 
+			num = i 
+  
+	return num 
+
+def get_average_colour_from_screen(screen_size: tuple=(1920,1080), step: int=40):
+	''' returns average value of (rbg) from screen
+
+	Arguments:
+		screen_size - pixel size of screen given in tuple (width, height)
+		step - number of pixels 
+			(calculating with small step would take a lot of time
+			and big step will not be effective and truthful)
+	'''
+	num_pixel = screen_size[0]//step * screen_size[1]//step
+
+	img = ImageGrab.grab()
+	imgNP = np.array(img)
+
+	im_arr = np.frombuffer(img.tobytes(), dtype=np.uint8)
+	im_arr = im_arr.reshape((img.size[1], img.size[0], 3))
+	r = g = b = 0
+	pixel_array = []
+	for y in range(0, screen_size[1], step):
+		for x in range(0, screen_size[0], step):
+			px = im_arr[y][x]
+
+			pixel_array.append([px[0], px[1], px[2]])
+
+	colour = most_frequent(pixel_array)
+	return colour
+
+
+# function of RAINBOW MODE (Natalia's Version)
+#
+#
